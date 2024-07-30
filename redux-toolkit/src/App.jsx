d4010288d224ecf9108ab6a2536086a0459dcdaa
subhonil-sardar <subhonilsardar@gmail.com>
2024-07-30 11:42:46 -0400

    My frist redux-toolkit app

---
+++ redux-toolkit/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CounterView from './features/counter/CounterView'
+
+function App() {
+
+  return (
+    <>
+         <h1>Counter App</h1>
+        <CounterView />
+    </>
+  )
+}
+
+export default App
